da7e68c76d93d6988255ec01d5ff98dd401cbd91
MarcusWang <partypie3091@gmail.com>
2023-01-04 15:33:08 +0800

    IOS_Final_Project_UpLoad

---
+++ IOS_FinalProject/AppView.swift
@@ -0,0 +1,21 @@
+//
+//  AppView.swift
+//  IOS_FinalProject
+//
+//  Created by User02 on 2020/1/5.
+//  Copyright © 2020 matcha. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
